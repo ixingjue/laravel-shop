f6afb8c6bd971c0e6b20472b2536063c707e04d9
ixingjue <826739558@qq.com>
2018-12-08 11:29:26 +0800

    用户界面 - 商品详情页

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,8 @@
 */
 
 Route::redirect('/', '/products')->name('root');
-Route::get('products','ProductsController@index')->name('products.index');
+Route::get('products', 'ProductsController@index')->name('products.index');
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 Auth::routes();
 Route::group(['middleware' => 'auth'], function () {
     Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
@@ -25,7 +26,8 @@ Route::group(['middleware' => 'auth'], function () {
         Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
         Route::get('user_addresses/{user_address}', 'UserAddressesController@edit')->name('user_addresses.edit');
         Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
-        Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
+        Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')
+            ->name('user_addresses.destroy');
     });
     //结束
 });
