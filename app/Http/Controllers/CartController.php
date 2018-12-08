44ea5544d95d85f16210c84fcd66a614305898ae
ixingjue <826739558@qq.com>
2018-12-08 20:38:17 +0800

    购物车页面

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\AddCartRequest;
+use App\Http\Requests\Request;
 use App\Models\CartItem;
+use App\Models\ProductSku;
 
 class CartController extends Controller
 {
     public function add(AddCartRequest $request)
     {
+        //@fixme 有机会实现未登录保存购物车的功能  https://laravel-china.org/topics/17397?#reply74136
         $user = $request->user();
         $skuId = $request->input('sku_id');
         $amount = $request->input('amount');
@@ -25,6 +28,18 @@ class CartController extends Controller
             $cart->productSku()->associate($skuId);
             $cart->save();
         }
+        return [];
+    }
+
+    public function index(Request $request)
+    {
+        $cartItems = $request->user()->cartItems()->with(['productSku.product'])->get();
+        return view('cart.index', ['cartItems' => $cartItems]);
+    }
+
+    public function remove(ProductSku $sku, Request $request)
+    {
+        $request->user()->cartItems()->where('product_sku_id', $sku->id)->delete();
         return [];
     }
 }
