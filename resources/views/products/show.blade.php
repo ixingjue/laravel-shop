b32b803502f08c63e813fe393534d26342998d60
ixingjue <826739558@qq.com>
2018-12-08 14:50:16 +0800

    Fixed favor bug

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -48,11 +48,18 @@
                                                     // 发起一个 post ajax 请求，请求 url 通过后端的 route() 函数生成。
                                                     axios.post('{{route('products.favor',['product' => $product->id])}}')
                                                         .then(function () {// 请求成功会执行这个回调
-                                                            swal('操作成功', '', 'success');
+                                                            swal('操作成功', '', 'success')
+                                                                .then(function () {
+                                                                    location.reload()
+                                                                })
                                                         }, function (error) { // 请求失败会执行这个回调
                                                             // 如果返回码是 401 代表没登录
                                                             if (error.response && error.response.status === 401) {
-                                                                swal('请先登录', '', 'error');
+                                                                swal('请先登录', '', 'error')
+                                                                // 此处增加代码实现跳转至用户登录页面
+                                                                    .then(function () {
+                                                                        location.href = '{{route('login')}}';
+                                                                    });
                                                             } else if (error.response && error.response.data.msg) {
                                                                 // 其他有 msg 字段的情况，将 msg 提示给用户
                                                                 swal(error.response.data.msg, '', 'error');
