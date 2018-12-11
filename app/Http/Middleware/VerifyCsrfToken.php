03026dc48205ea9ff1400c9e69aa5574ccd039d8
ixingjue <826739558@qq.com>
2018-12-11 21:41:23 +0800

    微信支付

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -15,5 +15,7 @@ class VerifyCsrfToken extends Middleware
         // 标题是 Page Expired。在 Laravel 里 Page Expired 通常代表 Post 请求缺少 CSRF Token 或者 CSRF Token 过期
         // 由于我们这个 URL 是给支付宝服务器调用的，肯定不会有 CSRF Token，所以需要把这个 URL 加到 CSRF 白名单里：
         'payment/alipay/notify',
+        // 微信支付
+        'payment/wechat/notify',
     ];
 }
