dc2943cdfe306fcc8e2ed40564785a012fb9cc6f
ixingjue <826739558@qq.com>
2018-12-12 18:04:04 +0800

    管理后台 - 订单列表

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -5,9 +5,9 @@ use Illuminate\Routing\Router;
 Admin::registerAuthRoutes();
 
 Route::group([
-    'prefix'        => config('admin.route.prefix'),
-    'namespace'     => config('admin.route.namespace'),
-    'middleware'    => config('admin.route.middleware'),
+    'prefix' => config('admin.route.prefix'),
+    'namespace' => config('admin.route.namespace'),
+    'middleware' => config('admin.route.middleware'),
 ], function (Router $router) {
 
     $router->get('/', 'HomeController@index');
@@ -15,6 +15,7 @@ Route::group([
     $router->get('products', 'ProductsController@index');
     $router->get('products/create', 'ProductsController@create');
     $router->post('products', 'ProductsController@store');
-    $router->get('products/{id}/edit','ProductsController@edit');
-    $router->put('products/{id}','ProductsController@update');
+    $router->get('products/{id}/edit', 'ProductsController@edit');
+    $router->put('products/{id}', 'ProductsController@update');
+    $router->get('orders', 'OrdersController@index')->name('admin.orders.index');
 });
