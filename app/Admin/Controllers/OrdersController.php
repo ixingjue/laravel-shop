12a8affb1a36b2577bfb71a81fa55a8e9a01edb5
ixingjue <826739558@qq.com>
2018-12-12 21:55:46 +0800

    增加消息提示

--- app/Admin/Controllers/OrdersController.php
+++ app/Admin/Controllers/OrdersController.php
@@ -41,6 +41,7 @@ class OrdersController extends Controller
         }
         return $content
             ->header('查看订单')
+            // body 方法可以接受 Laravel 的视图作为参数
             ->body(view('admin.orders.show', ['order' => $order]));
     }
 
