c2ec064bc402ddf650b6866b0663f4a209ecf6f3
ixingjue <826739558@qq.com>
2018-12-15 13:13:21 +0800

    评价商品

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\OrderReviewed;
 use App\Exceptions\InvalidRequestException;
 use App\Http\Requests\OrderRequest;
 use App\Http\Requests\Request;
+use App\Http\Requests\SendReviewRequest;
 use App\Jobs\CloseOrder;
 use App\Models\Order;
 use App\Models\ProductSku;
@@ -108,5 +110,48 @@ class OrdersController extends Controller
         //        return redirect()->back();
         // 我们把确认收货的操作从表单提交改成了 AJAX 请求，因此控制器中的返回值需要修改一下
         return $order;
+    }
+
+    public function review(Order $order)
+    {
+        // 校验数据
+        $this->authorize('own', $order);
+        // 判断是否已经支付
+        if (!$order->paid_at) {
+            throw new InvalidRequestException('该订单未支付，不可评价');
+        }
+        // 使用 load 方法加载关联数据，避免N+1性能问题
+        return view('orders.review', ['order' => $order->load(['items.productSku', 'items.product'])]);
+    }
+
+    public function sendReview(Order $order, SendReviewRequest $request)
+    {
+        // 校验权限
+        $this->authorize('own', $order);
+        if (!$order->paid_at) {
+            throw new InvalidRequestException('该订单未支付，不可评价');
+        }
+        // 判断是否已经评价
+        if ($order->reviewed) {
+            throw new InvalidRequestException('该订单已评价，不可重复提交');
+        }
+        $reviews = $request->input('reviews');
+        //开启事务
+        \DB::transaction(function () use ($reviews, $order) {
+            // 遍历用户提交的数据
+            foreach ($reviews as $review) {
+                $orderItem = $order->items()->find($review['id']);
+                //保存评分和评价
+                $orderItem->update([
+                    'rating' => $review['rating'],
+                    'review' => $review['review'],
+                    'reviewed_at' => Carbon::now(),
+                ]);
+            }
+            //将订单标记为已评价
+            $order->update(['reviewed' => true]);
+            event(new OrderReviewed($order));
+        });
+        return redirect()->back();
     }
 }
