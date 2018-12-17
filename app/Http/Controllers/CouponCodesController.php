e242037bd5d2a541f545a7f66105909af0e4bb36
ixingjue <826739558@qq.com>
2018-12-17 16:49:58 +0800

    优化优惠券模块

--- app/Http/Controllers/CouponCodesController.php
+++ app/Http/Controllers/CouponCodesController.php
@@ -9,12 +9,12 @@ use Carbon\Carbon;
 
 class CouponCodesController extends Controller
 {
-    public function show($code)
+    public function show($code, Request $request)
     {
         if (!$record = CouponCode::query()->where('code', $code)->first()) {
             throw new CouponCodeUnavailableException('优惠券不存在');
         }
-        $record->checkAvailable();
+        $record->checkAvailable($request->user());
         return $record;
     }
 }
