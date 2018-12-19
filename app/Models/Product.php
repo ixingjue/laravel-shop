99419f58e05c2d5fe5199d77a08d66374daf7c22
ixingjue <826739558@qq.com>
2018-12-19 15:09:12 +0800

    类目 Seeder文件

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -24,4 +24,9 @@ class Product extends Model
     {
         return $this->hasMany(ProductSku::class);
     }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
