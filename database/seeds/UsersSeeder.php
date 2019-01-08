d4048c74133f0178accf7e659912a789edd4938c
ixingjue <826739558@qq.com>
2019-01-08 17:53:46 +0800

    给id为1的用户设置默认用户名和密码

--- database/seeds/UsersSeeder.php
+++ database/seeds/UsersSeeder.php
@@ -13,5 +13,9 @@ class UsersSeeder extends Seeder
     {
         // 通过 factory 方法生成 100 个用户并保存到数据库中
         factory(\App\Models\User::class, 100)->create();
+        $user = \App\Models\User::query()->find(1);
+        $user->email = '826739558@qq.com';
+        $user->password = bcrypt('password');
+        $user->save();
     }
 }
