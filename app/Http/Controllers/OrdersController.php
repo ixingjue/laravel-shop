6730b4fc33d6245de48e80a3097d47082344b60a
ixingjue <826739558@qq.com>
2019-01-11 14:27:35 +0800

    订单模块调整

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -19,6 +19,7 @@ use Carbon\Carbon;
 use App\Models\UserAddress;
 use App\Services\CartService;
 use App\Exceptions\CouponCodeUnavailableException;
+use Monolog\Handler\IFTTTHandler;
 
 class OrdersController extends Controller
 {
@@ -121,6 +122,10 @@ class OrdersController extends Controller
         // 判断订单是否已付款
         if (!$order->paid_at) {
             throw new InvalidRequestException('该订单未支付，不可退款');
+        }
+        // 众筹订单不允许申请退款
+        if ($order->type === Order::TYPE_CROWDFUNDING) {
+            throw new InvalidRequestException('众筹订单不支持退款');
         }
         // 判断订单退款状态是否正确
         if ($order->refund_status !== Order::REFUND_STATUS_PENDING) {
